e528414a0826f3934df9fab29270789a86ffefb2
carminerizzi91-spec <carminerizzi91@gmail.com>
2026-05-04 13:11:41 +0200

    add: reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,13 +1,41 @@
 const connection = require('../data/db');
 
-function index(req, res){
-    res.send("index dei film");
+function index(req, res) {
+    const sql = 'SELECT * FROM movies'
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed!' });
+        res.json(results);
+    });
 
 }
 
-function show(req, res){
-    res.send("show dei film");
-    
+function show(req, res) {
+    // Id dall'url
+    const id = req.params.id
+
+    // query con segnaposto ?
+    const sql = 'SELECT * FROM movies WHERE id = ?';
+
+    // query con segnaposto ? per le review
+    const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    // chiamata esecuzione query film
+    connection.query(sql, [id], (err, movieResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (movieResults.length === 0) return res.status(404).json({ error: 'Movie not found' });
+
+        const movie = movieResults[0];
+        // chiamata esecuzione query recensione
+        connection.query(reviewsSql, [id], (err, reviewResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+
+            movie.reviews = reviewResults;
+            res.json(movie);
+        })
+        
+    })
+
 }
 
-module.exports = {index, show}
+module.exports = { index, show }
